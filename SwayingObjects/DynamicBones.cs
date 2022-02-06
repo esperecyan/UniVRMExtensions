e9519162d9ceee4ec45db17aae21b9d25ec6bd26
100の人 <100@pokemori.jp>
2022-02-06 14:16:04 +0900

    [SwayingObjectsConverter] DynamicBoneがインポートされているか否かを返すAPIを追加
    https://github.com/esperecyan/VRMConverterForVRChat/issues/31

--- SwayingObjects/DynamicBones.cs
+++ SwayingObjects/DynamicBones.cs
@@ -2,11 +2,20 @@ using System;
 
 namespace Esperecyan.UniVRMExtensions.SwayingObjects
 {
-    internal static class DynamicBones
+    public static class DynamicBones
     {
         internal static readonly Type DynamicBoneType = Type.GetType("DynamicBone, Assembly-CSharp");
         internal static readonly Type DynamicBoneColliderType = Type.GetType("DynamicBoneCollider, Assembly-CSharp");
         internal static readonly Type DynamicBoneColliderBaseListType
             = Type.GetType("System.Collections.Generic.List`1[[DynamicBoneColliderBase, Assembly-CSharp]]");
+
+        /// <summary>
+        /// DynamicBoneアセットがインポートされていれば <c>true</c> を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsImported()
+        {
+            return DynamicBoneType != null;
+        }
     }
 }
