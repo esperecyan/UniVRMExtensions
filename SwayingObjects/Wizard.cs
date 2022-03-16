ec753e9f466162113d7616c94a07d6f6fa618157
100の人 <100@pokemori.jp>
2022-03-16 09:41:14 +0900

    不要なDebug.Log()をコミットしてしまっていたのを修正

--- SwayingObjects/Wizard.cs
+++ SwayingObjects/Wizard.cs
@@ -375,7 +375,6 @@ public class Example : MonoBehaviour
                             break;
                         case Direction.VRMSpringBonesToDynamicBones:
                             Utilities.DestroyVRMSpringBones(source, sourceIsAsset);
-                            Debug.Log(1223);
                             break;
                     }
 
